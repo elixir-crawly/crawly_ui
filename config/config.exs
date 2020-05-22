4230e39dc80aad8d4bacbde863d484f4715f7220
Oleg Tarasenko <oltarasenko@gmail.com>
2020-05-22 14:07:01 +0200

    Group queries

--- config/config.exs
+++ config/config.exs
@@ -37,10 +37,7 @@ config :crawly_ui, CrawlyUI.Scheduler,
          {"* */1 * * *",      {CrawlyUI.Manager, :update_job_status, []}},
 
          # Every minute
-         {"*/1 * * * *",      {CrawlyUI.Manager, :update_item_counts, []}},
-         {"*/1 * * * *",      {CrawlyUI.Manager, :update_crawl_speeds, []}},
-         {"*/1 * * * *",      {CrawlyUI.Manager, :update_run_times, []}},
-       ]
+         {"*/4 * * * *",      {CrawlyUI.Manager, :update_running_jobs, []}}
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
