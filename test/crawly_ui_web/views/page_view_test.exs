fdff4a8297c38caf84259713d9f325e9da1d7c25
Phuong Van <phuong.van@erlang-solutions.com>
2020-09-08 11:49:05 +0200

    add job view test

--- test/crawly_ui_web/views/page_view_test.exs
+++ test/crawly_ui_web/views/page_view_test.exs
@@ -1,3 +1,9 @@
 defmodule CrawlyUIWeb.PageViewTest do
   use CrawlyUIWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  test "show index.html" do
+    assert render_to_string(CrawlyUIWeb.PageView, "index.html", []) =~ "Web Crawling Dashboard."
+  end
 end
