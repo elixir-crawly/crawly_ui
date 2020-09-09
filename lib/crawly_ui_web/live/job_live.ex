e06523a19508e426e703c687ecadb114e0fa7a26
Phuong Van <phuong.van@erlang-solutions.com>
2020-09-09 11:48:15 +0200

    [WIP] Move job pick node to liveview

--- lib/crawly_ui_web/live/job_live.ex
+++ lib/crawly_ui_web/live/job_live.ex
@@ -3,66 +3,64 @@ defmodule CrawlyUIWeb.JobLive do
 
   alias CrawlyUI.Manager
 
-  def render(assigns) do
-    CrawlyUIWeb.JobView.render("index.html", assigns)
+  @abandon_state "abandoned"
+
+  def render(%{template: template} = assigns) do
+    CrawlyUIWeb.JobView.render(template, assigns)
   end
 
-  def mount(_param, %{"jobs" => jobs}, socket) do
-    if connected?(socket), do: Process.send_after(self(), :update, 1000)
+  def mount(_param, %{"template" => "index.html" = template, "jobs" => jobs}, socket) do
+    live_update(socket, :update_job, 1000)
 
-    jobs =
-      for %{id: job_id} <- jobs do
-        job = Manager.get_job!(job_id)
+    jobs = Enum.map(jobs, &may_update_job/1)
 
-        state = get_state(job)
-        run_time = get_runtime(job)
-        crawl_speed = get_crawl_speed(job)
-        items_count = get_items_count(job)
-
-        Map.merge(job, %{
-          run_time: run_time,
-          crawl_speed: crawl_speed,
-          items_count: items_count,
-          state: state
-        })
-      end
-
-    {:ok, assign(socket, jobs: jobs)}
+    {:ok, assign(socket, template: template, jobs: jobs)}
   end
 
-  def handle_info(:update, socket) do
+  def mount(_param, %{"template" => "pick_node.html" = template, "nodes" => nodes}, socket) do
+    live_update(socket, :pick_node, 1000)
+    {:ok, assign(socket, template: template, nodes: nodes)}
+  end
+
+  def handle_info(:update_job, socket) do
     live_jobs = socket.assigns.jobs
 
     # If any of the jobs are running or in new state then we should keep updating
     # every 100ms else, refresh every second
     updated_live_jobs =
       if need_update?(live_jobs) do
-        if connected?(socket), do: Process.send_after(self(), :update, 100)
-
-        for job <- live_jobs do
-          may_update_job(job)
-        end
+        live_update(socket, :update_job, 100)
+        Enum.map(live_jobs, &may_update_job/1)
       else
-        if connected?(socket), do: Process.send_after(self(), :update, 1000)
+        live_update(socket, :update_job, 1000)
         live_jobs
       end
 
     {:noreply, assign(socket, jobs: updated_live_jobs)}
   end
 
-  defp need_update?([]), do: false
-
-  defp need_update?(jobs) do
-    Enum.any?(jobs, &(&1.state == "running" or &1.state == "new"))
+  def handle_info(:pick_node, socket) do
+    live_update(socket, :pick_node, 1000)
+    nodes = Node.list()
+    {:noreply, assign(socket, nodes: nodes)}
   end
 
+  defp live_update(socket, state, time) do
+    if connected?(socket), do: Process.send_after(self(), state, time)
+  end
+
+  defp need_update?([]), do: false
+  defp need_update?(jobs), do: Enum.any?(jobs, &(&1.state == "running" or &1.state == "new"))
+
   defp may_update_job(%{id: job_id} = live_job) do
-    # if job state is "running" then update the fields
     job = Manager.get_job!(job_id)
     state = get_state(job)
 
     case state do
-      "running" ->
+      @abandon_state ->
+        live_job
+
+      _ ->
         run_time = get_runtime(job)
         crawl_speed = get_crawl_speed(job)
         items_count = get_items_count(job)
@@ -73,24 +71,15 @@ defmodule CrawlyUIWeb.JobLive do
           items_count: items_count,
           state: state
         })
-
-      _ ->
-        live_job
     end
   end
 
   defp get_state(job) do
-    case Manager.job_state(job) do
-      "running" = state ->
-        if Manager.is_job_abandoned(job) do
-          "abandoned"
-        else
-          state
-        end
-
-      state ->
-        state
+    if Manager.is_job_abandoned(job) do
+      Manager.update_state(job, @abandon_state)
     end
+
+    Manager.job_state(job)
   end
 
   defp get_runtime(job) do
@@ -105,12 +94,7 @@ defmodule CrawlyUIWeb.JobLive do
     end
   end
 
-  defp get_crawl_speed(job) do
-    speed = Manager.crawl_speed(job)
-    "#{speed} items/min"
-  end
+  defp get_crawl_speed(job), do: "#{Manager.crawl_speed(job)} items/min"
 
-  defp get_items_count(job) do
-    Manager.count_items(job)
-  end
+  defp get_items_count(job), do: Manager.count_items(job)
 end
