80c72b0cf2e812b166619c138aad23438b9c2e77
Phuong Van <phuong.van@erlang-solutions.com>
2020-09-09 12:16:26 +0200

    Shorten the code

--- lib/crawly_ui_web/live/job_live.ex
+++ lib/crawly_ui_web/live/job_live.ex
@@ -3,16 +3,12 @@ defmodule CrawlyUIWeb.JobLive do
 
   alias CrawlyUI.Manager
 
-  @abandon_state "abandoned"
-
   def render(%{template: template} = assigns) do
     CrawlyUIWeb.JobView.render(template, assigns)
   end
 
   def mount(_param, %{"template" => "index.html" = template, "jobs" => jobs}, socket) do
     live_update(socket, :update_job, 1000)
-
-    jobs = Enum.map(jobs, &may_update_job/1)
 
     {:ok, assign(socket, template: template, jobs: jobs)}
   end
@@ -27,16 +23,17 @@ defmodule CrawlyUIWeb.JobLive do
 
     # If any of the jobs are running or in new state then we should keep updating
     # every 100ms else, refresh every second
-    updated_live_jobs =
-      if need_update?(live_jobs) do
-        live_update(socket, :update_job, 100)
-        Enum.map(live_jobs, &may_update_job/1)
-      else
-        live_update(socket, :update_job, 1000)
-        live_jobs
-      end
 
-    {:noreply, assign(socket, jobs: updated_live_jobs)}
+    if need_update?(live_jobs) do
+      live_update(socket, :update_job, 100)
+
+      Manager.update_job_status()
+      Manager.update_running_jobs()
+    else
+      live_update(socket, :update_job, 1000)
+    end
+
+    {:noreply, assign(socket, jobs: Manager.list_jobs())}
   end
 
   def handle_info(:pick_node, socket) do
@@ -51,50 +48,4 @@ defmodule CrawlyUIWeb.JobLive do
 
   defp need_update?([]), do: false
   defp need_update?(jobs), do: Enum.any?(jobs, &(&1.state == "running" or &1.state == "new"))
-
-  defp may_update_job(%{id: job_id} = live_job) do
-    job = Manager.get_job!(job_id)
-    state = get_state(job)
-
-    case state do
-      @abandon_state ->
-        live_job
-
-      _ ->
-        run_time = get_runtime(job)
-        crawl_speed = get_crawl_speed(job)
-        items_count = get_items_count(job)
-
-        Map.merge(live_job, %{
-          run_time: run_time,
-          crawl_speed: crawl_speed,
-          items_count: items_count,
-          state: state
-        })
-    end
-  end
-
-  defp get_state(job) do
-    if Manager.is_job_abandoned(job) do
-      Manager.update_state(job, @abandon_state)
-    end
-
-    Manager.job_state(job)
-  end
-
-  defp get_runtime(job) do
-    case Manager.run_time(job) do
-      result when result > 60 ->
-        time_in_hours = (result / 60) |> Float.round(2)
-        "#{time_in_hours} hours"
-
-      result ->
-        time_in_minutes = result |> Kernel.trunc()
-        "#{time_in_minutes} min"
-    end
-  end
-
-  defp get_crawl_speed(job), do: "#{Manager.crawl_speed(job)} items/min"
-
-  defp get_items_count(job), do: Manager.count_items(job)
 end
