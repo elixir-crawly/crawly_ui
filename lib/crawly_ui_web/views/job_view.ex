833a9e79b2335a699c267fd3a9348be719a12e7f
Phuong Van <phuong.van@erlang-solutions.com>
2020-09-15 11:56:36 +0200

    parse run time in job view

--- lib/crawly_ui_web/views/job_view.ex
+++ lib/crawly_ui_web/views/job_view.ex
@@ -1,3 +1,18 @@
 defmodule CrawlyUIWeb.JobView do
   use CrawlyUIWeb, :view
+
+  def render_run_time(run_time) do
+    case run_time do
+      nil ->
+        "-"
+
+      result when result > 60 ->
+        time_in_hours = (result / 60) |> Float.round(2)
+        "#{time_in_hours} hours"
+
+      result ->
+        time_in_minutes = result |> Kernel.trunc()
+        "#{time_in_minutes} min"
+    end
+  end
 end
--- test/crawly_ui_web/views/job_view_test.exs
+++ test/crawly_ui_web/views/job_view_test.exs
@@ -1,3 +1,24 @@
 defmodule CrawlyUIWeb.JobViewTest do
   use CrawlyUIWeb.ConnCase
+  import CrawlyUI.DataCase
+
+  import Phoenix.View
+
+  test "render runtime when job run time is nil" do
+    job = insert_job(%{run_time: nil})
+
+    assert render_to_string(CrawlyUIWeb.JobView, "index.html", jobs: [job]) =~ "-"
+  end
+
+  test "render runtime when job run time is less than an hour" do
+    job = insert_job(%{run_time: 10})
+
+    assert render_to_string(CrawlyUIWeb.JobView, "index.html", jobs: [job]) =~ "10 min"
+  end
+
+  test "render runtime when job run time is more than an hour" do
+    job = insert_job(%{run_time: 90})
+
+    assert render_to_string(CrawlyUIWeb.JobView, "index.html", jobs: [job]) =~ "1.5 hours"
+  end
 end
