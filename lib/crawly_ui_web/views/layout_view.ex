a25164de6e267dbef63c4755af33c0bad9fc25af
Oleg Tarasenko <oltarasenko@gmail.com>
2020-09-28 13:23:26 +0200

    Add possibility to add GA TACKING

--- lib/crawly_ui_web/views/layout_view.ex
+++ lib/crawly_ui_web/views/layout_view.ex
@@ -1,3 +1,23 @@
 defmodule CrawlyUIWeb.LayoutView do
   use CrawlyUIWeb, :view
+
+  def ga_tag() do
+    case System.get_env("GA_TRACKING") do
+      nil ->
+        ""
+
+      tracking_code ->
+        """
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=#{tracking_code}"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '#{tracking_code}');
+        </script>
+        """
+    end
+  end
 end
