0f9c46a7cf8bfbf2d57de0e26e31f663199b0d45
Oleg Tarasenko <oltarasenko@gmail.com>
2020-10-12 13:19:27 +0200

    Allow to inject google tag manager code instead of google analytics

--- lib/crawly_ui_web/views/layout_view.ex
+++ lib/crawly_ui_web/views/layout_view.ex
@@ -1,22 +1,35 @@
 defmodule CrawlyUIWeb.LayoutView do
   use CrawlyUIWeb, :view
 
-  def ga_tag() do
+  def ga_js_tag() do
     case System.get_env("GA_TRACKING") do
       nil ->
         ""
 
       tracking_code ->
         """
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=#{tracking_code}"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','#{tracking_code}');</script>
+        <!-- End Google Tag Manager -->
+        """
+    end
+  end
 
-          gtag('config', '#{tracking_code}');
-        </script>
+  def ga_nojs_tag() do
+    case System.get_env("GA_TRACKING") do
+      nil ->
+        ""
+
+      tracking_code ->
+        """
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=#{tracking_code}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         """
     end
   end
