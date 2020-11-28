44e5837d965373c327f261d69a124d9ce53068f8
Oleg Tarasenko <oltarasenko@gmail.com>
2020-11-28 21:29:45 +0100

    Allow scheduling generic spiders via general window

--- lib/crawly_ui_web/live/schedule_live.ex
+++ lib/crawly_ui_web/live/schedule_live.ex
@@ -2,6 +2,7 @@ defmodule CrawlyUIWeb.ScheduleLive do
   use Phoenix.LiveView
 
   alias CrawlyUI.SpiderManager
+  alias CrawlyUI.Manager
 
   def render(%{template: template} = assigns) do
     CrawlyUIWeb.JobView.render(template, assigns)
@@ -33,7 +34,12 @@ defmodule CrawlyUIWeb.ScheduleLive do
     node = socket.assigns.node
     spiders = SpiderManager.list_spiders(node)
 
-    {:noreply, assign(socket, spiders: spiders)}
+    generic_spiders = Enum.map(
+      Manager.list_spiders(page_size: 100),
+      fn spider -> spider.name end
+    )
+
+    {:noreply, assign(socket, generic_spiders: generic_spiders, spiders: spiders ++ generic_spiders)}
   end
 
   def handle_event("spider_picked", %{"node" => node}, socket) do
@@ -46,6 +52,13 @@ defmodule CrawlyUIWeb.ScheduleLive do
   def handle_event("schedule_spider", %{"spider" => spider}, socket) do
     node = socket.assigns.node
 
+    case spider in socket.assigns.generic_spiders do
+      true ->
+        CrawlyUI.create_spider(node, spider)
+      false ->
+        :ok
+    end
+    
     case SpiderManager.start_spider(node, spider) do
       {:ok, :started} ->
         {:noreply,
